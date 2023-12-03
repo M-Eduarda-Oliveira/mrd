aacec73037297b33098eeb88d0eba5b27c93aaa4
Maria Eduarda Oliveira <oliveiradelimamariaeduarda@gmail.com>
2023-12-02 22:59:40 -0300

    refact: melhorias no código

--- src/services/categorias_gastos.py
+++ src/services/categorias_gastos.py
@@ -3,50 +3,49 @@ from utils.validacao_input import input_valido
 from utils.limpar_console import limparConsole
 from data.registros_categorias import registrosGerais
 from utils.mensagem import layoutMensagem
+
 # Menu da funcionalidade 01 - Funcionalidade: Definir categorias de gasto
 def menu_definir_categorias():
-    while True:
-        layoutMensagem("Olá, seja bem-vindo a nossa funcionalidade de definição de categorias de despesas e seus limites de gasto!\nNesta funcionalidade você poderá definir e editar essas categorias de forma altamente personalizada \nse adequando assim a sua forma de gerir o seu rico dinheirinho.\nAproveite ;)")
-        print("Menu de Categorias\n 1. Definir Orçamento Total Mensal\n 2. Editar Orçamento Total Mensal\n 3. Criar Categorias de Despesas\n 4. Editar Categorias de Despesas\n 5. Deletar categoria de gasto \n 6. Visualizar orçamento total e categorias \n 7. Voltar ao Menu Principal")
-        escolha = input_valido("Escolha uma opção: ")
+    layoutMensagem("Olá, seja bem-vindo a nossa funcionalidade de definição de categorias de despesas e seus limites de gasto!\nNesta funcionalidade você poderá definir e editar essas categorias de forma altamente personalizada \nse adequando assim a sua forma de gerir o seu rico dinheirinho.\nAproveite ;)")
+    print("Menu de Categorias\n 1. Definir Orçamento Total Mensal\n 2. Editar Orçamento Total Mensal\n 3. Criar Categorias de Despesas\n 4. Editar Categorias de Despesas\n 5. Deletar categoria de gasto \n 6. Visualizar orçamento total e categorias \n 7. Voltar ao Menu Principal")
+    escolha = input_valido("Escolha uma opção: ")
 
-        if escolha == 1:
-            limparConsole()
-            definir_orcamento()
-        elif escolha == 2:
-            limparConsole()
-            editar_orcamento()
-        elif escolha == 3:
-            limparConsole()
-            criar_categoria()
-        elif escolha == 4:
-            limparConsole()
-            editar_categoria()
-        elif escolha == 5:
-            limparConsole()
-            deletarCategoria()
-        elif escolha == 6:
-            limparConsole()
-            visualizarDados()
-        elif escolha == 7:
-            limparConsole()
-            break
-        else:
-            print("Opção inválida, tente novamente.")
-            limparConsole()
-
+    if escolha == 1:
+        limparConsole(0)
+        definir_orcamento()
+    elif escolha == 2:
+        limparConsole(0)
+        editar_orcamento()
+    elif escolha == 3:
+        limparConsole(0)
+        criar_categoria()
+    elif escolha == 4:
+        limparConsole(0)
+        editar_categoria()
+    elif escolha == 5:
+        limparConsole(0)
+        deletarCategoria()
+    elif escolha == 6:
+        limparConsole(0)
+        visualizarDados()
+    elif escolha == 7:
+        from menu_principal import menu_principal
+        limparConsole(0)
+        menu_principal()
+    else:
+        print("Opção inválida, tente novamente.")
+        limparConsole(1)
 # Função: Definir orçamento mensal total
 def definir_orcamento():
     try:
         orcamento = float(input("Digite o valor do orçamento total mensal: "))
         registrosGerais['orcamentoMensal'] = orcamento
         print("Orçamento definido com sucesso.")
-        sleep(1)
-        limparConsole()
+        limparConsole(2)
+        menu_definir_categorias()
     except ValueError:
         print("Por favor, insira um número válido.")
-        sleep(1)
-        limparConsole()
+        limparConsole(2)
 
 
 # Função: Editar orçamento mensal total 
@@ -61,17 +60,17 @@ def editar_orcamento():
         # with open("orçamento.txt", "w") as arquivo:
         #     arquivo.write(str(novo_orcamento))
         print("Orçamento atualizado com sucesso.")
-        sleep(1)
-        limparConsole()
+        limparConsole(2)
+        menu_definir_categorias()
+
 
     except FileNotFoundError:
         print("Orçamento não definido. Defina um orçamento primeiro.")
-        sleep(1)
-        limparConsole()
+        limparConsole(2)
+        menu_definir_categorias()
     except ValueError:
         print("Por favor, insira um número válido.")
-        sleep(1)
-        limparConsole()
+        limparConsole(2)
 
 # Função: Criar nova categoria de gasto
 def criar_categoria():
@@ -84,11 +83,10 @@ def criar_categoria():
     # pode ser feito com array ou abjeto 
     registrosGerais['categorias'].append({'nome':nova_categoria, 'limite': limite, 'gasto': gasto})
     print("Categoria criada com sucesso.")
-    sleep(1)
-    limparConsole()
+    limparConsole(2)
     continuar = input("Deseja continuar e criar mais categorias? Você pode responder com S para sim e N para não ;)\n").upper()
-    while (continuar == 'S'):
-        limparConsole()
+    if (continuar == 'S'):
+        limparConsole(0)
         print('============================================================')
         print("As categorias que você criou até agora são:")
         for (categoria) in registrosGerais['categorias']:
@@ -119,20 +117,20 @@ def editar_categoria():
         registrosGerais['categorias'][categoriaEditar]['nome'] = novoNome
         registrosGerais['categorias'][categoriaEditar]['limite'] = novoLimite
         print('Categoria editada com sucesso.')
-        sleep(1)
         continuar = input("Deseja continuar e criar mais categorias? Você pode responder com S para sim e N para não ;)\n").upper()
-        limparConsole()
+        limparConsole(0)
         while (continuar == 'S'):
-            limparConsole()
+            limparConsole(0)
             editar_categoria()
         else:
+            limparConsole(0)
             menu_definir_categorias()
     except FileNotFoundError:
         print("Nenhum dado de gasto encontrado.")
         sleep(1)
         menu_definir_categorias()
     print("Categoria editada com sucesso.")
-    limparConsole()
+    limparConsole(1)
 
 def deletarCategoria():
     print('============================================================')
@@ -144,7 +142,9 @@ def deletarCategoria():
     categoriaDeletar = int(input("Digite o número da categoria que deseja deletar: "))
     registrosGerais['categorias'].pop(categoriaDeletar)
     print("Categoria deletada com sucesso.")
-    limparConsole()
+    limparConsole(1)
+    menu_definir_categorias()
+
 
 def visualizarDados():
     print('============================================================')
@@ -159,4 +159,4 @@ def visualizarDados():
         menu_definir_categorias()
     else:
         print("Digite uma opção válida.")
-    limparConsole()
+    limparConsole(3)
